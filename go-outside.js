0dbadc8d8e99fbf67956be66584823f6d7a0d215
emi-hi <emily.hillier@gmail.com>
2019-01-27 23:31:19 +0000

    adds conditional exercises

---
+++ go-outside.js
@@ -0,0 +1,16 @@
+var raining = true;
+var temperature = 12;
+if(raining) {
+  console.log("don't forget your umbrella!");
+}
+
+if(temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15){
+  console.log("short sleeves won't cut it.");
+} else {
+  console.log("short sleeves are fine");
+}
+
+
+console.log("Now you're ready to go outside!");
