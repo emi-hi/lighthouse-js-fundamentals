2fe0a92332e1953324ebba7338c311c9361dc10e
emi-hi <emily.hillier@gmail.com>
2019-01-28 00:08:59 +0000

    added some more exercises

---
+++ age.js
@@ -0,0 +1,7 @@
+let theNumber = Number(prompt("Pick a number!"));
+  if (!Number.isNaN(theNumber)) {
+  debug("your number is the square root of " +
+    theNumber * theNumber);
+} else {
+    debug("hey. give me a number?");
+}
---
+++ areacalculators.js
@@ -0,0 +1,25 @@
+function calculateRectangleArea(length, width) {
+  if (length > 0 && width > 0) {
+    return length * width;
+  } else if (length < 0 || width < 0){
+    return undefined;
+  }
+}
+
+function calculateTriangleArea(base, height){
+  if (base > 0 && height > 0){
+    return base * height / 2;
+  } else if (base < 0 || height < 0){
+    return undefined;
+  }
+}
+
+function calculateCircleArea(radius){
+  if (radius > 0){
+    return Math.PI * (radius * radius);
+  } else if (radius < 0){
+    return undefined;
+  }
+}
+console.log(calculateCircleArea(10));
+console.log(15 === "15")
